13ad67ae90f9604231b53c0893ce9146221a4f08
Coderbeth <106888733+Coderbeth@users.noreply.github.com>
2023-01-26 12:14:03 +0100

    Add files via upload

---
+++ mathworks.java
@@ -0,0 +1,101 @@
+public class mathworks{
+    public static void main(String[] args) {
+        System.out.println("OTITODIRINNA BETHEL EEATOR 19CJ025789");
+        System.out.println("Square root: " + Math.sqrt(89));
+        System.out.println("Absolute:" + Math.abs(8-9));
+        System.out.println("Raise to power: " + Math.pow(8, 9));
+        System.out.println("Exponent: " + Math.exp(6));
+        System.out.println("Cube root: " + Math.cbrt(89));
+  
+  
+if (8 > 9) {
+  System.out.println("8 is greater than 9");
+} 
+else {
+  System.out.println("9 is greater than 8");
+}
+  int m0 =8;
+        int m1 =9;
+        String smaller = m1==m0 ? "m1 is equal to m0" : "m1 is not equal to m0";
+        System.out.println(smaller+ "\n");
+
+         smaller = m1>m0 ? "m1 is greater than m0" : "m1 is not greater than to m0";
+        System.out.println(smaller+ "\n");
+
+         smaller = m1<m0 ? "m1 is less than m0" : "m1 is not less than to m0";
+        System.out.println(smaller+ "\n");
+
+        smaller = ((m1>1)||(m0>9)) ? "the or statement returns true" : "the or statement returns false";
+        System.out.println(smaller+ "\n");
+
+        smaller = ((m1>1)&&(m0>9)) ? "the and statement returns true" : "the and statement returns false";
+        System.out.println(smaller+ "\n");
+
+        smaller = ((m1!=m0)) ? "the not statement returns true" : "the not statement returns false";
+        System.out.println(smaller+ "\n");
+
+        
+        
+        System.out.println("m1 ^ m0 returns" +( m1^m0 )+ "\n");
+
+       
+        int cycler = 5;
+        switch(cycler){
+            case 1: System.out.println("this is case 1");
+            break;
+            case 3:System.out.println("this is case 3");
+            break;
+            case 5:System.out.println("this is case 5");
+            break;
+            case 6:System.out.println("this is case 6");
+            break;
+            default:System.out.println("this is default case");
+        }
+     //Switch Statement
+    int day = 10;
+    switch (day) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Enter a valid day of the week");
+
+        for(int i = 0; i <=99;i++){
+            if(i==89){
+                System.out.println(i);
+int x = 8;
+int y = 9;
+String result = (x <= y) ? "Good day." : "Good evening.";
+System.out.println(result);
+
+System.out.println(x>y);
+System.out.println(x<y);
+System.out.println(x==y);
+
+
+System.out.println(8>9 && 8<9);
+
+System.out.println(!(8>9 && 8<9));
+
+  }
+}
+  }
+  }}
